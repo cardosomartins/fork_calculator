a2235192833e10ffda408460fff799395b801d88
Paulo Martins <paulohcmartins@hotmail.com>
2024-01-19 23:44:22 -0300

    EventListener addition to each button

--- js/controller/script.js
+++ js/controller/script.js
@@ -1,33 +1,57 @@
 class calculator{
     constructor(){
-        this.operation;
+        this._buttonsEl = document.querySelectorAll(".btn");
         this._operation;
         this.init();
     }
 
     init(){
 
-        let calculatorMethod = () => {
-            this.calculatorStart()
-        };
-
-        let intervalId = setInterval(calculatorMethod, 500);
-
-        setTimeout(() => {clearInterval(intervalId)}, 1000);     
         
+        // let intervalId = setInterval(() => {this.calculatorMethod()}, 500);
+
+        // this.calculatorMethod();
+
+        // setTimeout(() => {clearInterval(intervalId)}, 1000);     
+        
+        this.showEachElement();
+    
      
     }
 
+    carlinhos(){
+        console.log("carlinhos diz oi!")
+    }
+
+    showEachElement(){
+        this._buttonsEl.forEach( button => {
+            button.addEventListener("click", this.carlinhos)
+        });
+
+    }
+
+
     calculatorStart(){
-        this._operation = 5;
+        this._operation = 7;
         console.log(this._operation);
     }
+
+    calculatorMethod(){ 
+
+        this.calculatorStart();
+        
+    }
+
+    
+    
+
+
     
     get operation(){
         return this._operation;
     }
     set operation(value){
-        _operation = value;
+        this._operation = value;
     }
 }
 
